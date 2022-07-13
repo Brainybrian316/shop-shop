08924f9f1a3bc3b6d34f744232087d3e4b63c303
Brainybrian316 <brainybrian316@gmail.com>
2022-07-13 02:03:46 -0400

    schema and src

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-
-
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
-  const { cart } = state;
+  const {
+    image,
+    name,
+    _id,
+    price,
+    quantity
+  } = item;
+
+  const { cart } = state
 
   const addToCart = () => {
-    // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-    // if there was a match, call UPDATE with a new purchase quantity
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
@@ -33,16 +37,7 @@ function ProductItem(item) {
       });
       idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
-  };
-  
-
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+  }
 
   return (
     <div className="card px-1 py-1">
